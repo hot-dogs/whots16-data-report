2e9997e70443dbdab9117f7a3882c814c1187e98
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-04-03 16:39:00 -1000

    create mystyle.tex.txt for preamble

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,6 +95,8 @@ html_theme_options = {
 # -- Options for LaTeX output ---------------------------------------------
 latex_engine = 'pdflatex'
 
+latex_additional_files = ["latex_templates/mystyle.tex.txt", "figures/logos/all_whot_report.png"]
+
 latex_elements = {
     'papersize': 'a4paper',
     # Sonny, Lenny, Glenn, Conny, Rejne, Bjarne and Bjornstrup # 'fncychap': '\\usepackage[Lenny]{fncychap}',
@@ -105,88 +107,7 @@ latex_elements = {
 
     # Additional stuff for the LaTeX preamble.
     'preamble': r'''
-        %%%%%%%%%%%%%%%%%%%% Meher %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%% add number to subsubsection 2=subsection, 3=subsubsection
-        %%% below subsubsection is not good idea.
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        \setcounter{secnumdepth}{3}
-        %
-        %%%% Table of content upto 2=subsection, 3=subsubsection
-        \setcounter{tocdepth}{3}
-
-        \usepackage{amsmath,amsfonts,amssymb,amsthm}
-        
-        % Include Imagesi
-        \usepackage{graphicx}
-
-%       %%% Reduce spaces for Table of contents, figures and tables
-%       %%% It is used "\addtocontents{toc}{\vskip -1.2cm}" etc. in the document
-%       \usepackage[notlot,nottoc,notlof]{}
-
-%       \usepackage{color}
-%       \usepackage{transparent}
-%       \usepackage{eso-pic}
-%       \usepackage{lipsum}
-
-%       \usepackage{footnotebackref} %%link at the footnote to go to the place of footnote in the text
-
-%       %% spacing between line
-%       \usepackage{setspace}
-%       %%%%\onehalfspacing
-%       %%%%\doublespacing
-%       \singlespacing
-
-
-%       %%%%%%%%%%% datetime
-%       \usepackage{datetime}
-
-%       \newdateformat{MonthYearFormat}{%
-%           \monthname[\THEMONTH], \THEYEAR}
-
-
-%       %% RO, LE will not work for 'oneside' layout.
-%       %% Change oneside to twoside in document class
-%       \usepackage{fancyhdr}
-%       \pagestyle{fancy}
-%       \fancyhf{}
-
-%       %%% Alternating Header for oneside
-%       \fancyhead[L]{\ifthenelse{\isodd{\value{page}}}{ \small \nouppercase{\leftmark} }{}}
-%       \fancyhead[R]{\ifthenelse{\isodd{\value{page}}}{}{ \small \nouppercase{\rightmark} }}
-
-%       %%% Alternating Header for two side
-%       %\fancyhead[RO]{\small \nouppercase{\rightmark}}
-%       %\fancyhead[LE]{\small \nouppercase{\leftmark}}
-
-%       %% for oneside: change footer at right side. If you want to use Left and right then use same as header defined above.
-%       \fancyfoot[R]{\ifthenelse{\isodd{\value{page}}}{{\tiny Meher Krishna Patel} }{\href{http://pythondsp.readthedocs.io/en/latest/pythondsp/toc.html}{\tiny PythonDSP}}}
-
-%       %%% Alternating Footer for two side
-%       %\fancyfoot[RO, RE]{\scriptsize Meher Krishna Patel (mekrip@gmail.com)}
-
-%       %%% page number
-%       \fancyfoot[CO, CE]{\thepage}
-
-%       \renewcommand{\headrulewidth}{0.5pt}
-%       \renewcommand{\footrulewidth}{0.5pt}
-
-%       \RequirePackage{tocbibind} %%% comment this to remove page number for following
-%       \addto\captionsenglish{\renewcommand{\contentsname}{Table of contents}}
-%       \addto\captionsenglish{\renewcommand{\listfigurename}{List of figures}}
-%       \addto\captionsenglish{\renewcommand{\listtablename}{List of tables}}
-%       \addto\captionsenglish{\renewcommand{\listtablename}{List of tables}} %%% Heading for TOC
-
-
-%       %%reduce spacing for itemize
-%       \usepackage{enumitem}
-%       \setlist{nosep}
-
-%       %%%%%%%%%%% Quote Styles at the top of chapter
-%       \usepackage{epigraph}
-%       \setlength{\epigraphwidth}{0.8\columnwidth}
-%       \newcommand{\chapterquote}[2]{\epigraphhead[60]{\epigraph{\textit{#1}}{\textbf {\textit{--#2}}}}}
-%       %%%%%%%%%%% Quote for all places except Chapter
-%       \newcommand{\sectionquote}[2]{{\quote{\textit{``#1''}}{\textbf {\textit{--#2}}}}}
+        \input{mystyle.tex.txt}
     ''',
 
     'maketitle': r'''
@@ -195,25 +116,24 @@ latex_elements = {
         \begin{titlepage}
             \centering
 
-            \vspace*{40mm} %%% * is used to give space from top
-            {\scshape\Large {University of Hawaii at Manoa} \par}
+            %\vspace*{2mm} %%% * is used to give space from top
+            {\scshape\Huge \textbf{University of Hawaii at Manoa} \par}
+            
             \rule{\textwidth}{0.4pt}\vspace*{-\baselineskip}\vspace{3.2pt} 
             \rule{\textwidth}{1.6pt}
             
             \vspace{1mm}
             
-            \hbox{\hspace{0em}\includegraphics[width=16cm]{all_whot_report.png}}
+            \hbox{\hspace{0em}\includegraphics[width=18cm]{all_whot_report.png}}
             \rule{\textwidth}{1.6pt}\vspace*{-\baselineskip}\vspace*{2pt} % Thick 
             \rule{\textwidth}{0.4pt} % Thin horizontal rule
             
-            {\Large \textbf{Hydrographic Observations At the Woods Hole Oceanographic 
+            {\Huge \textbf{Hydrographic Observations At the Woods Hole Oceanographic 
             Institution Hawaii Ocean Time-series Site: 2019 - 2020} \par}
             
-            by\par
-
-            \vspace{0mm}
+            {\Large {by}\par}
             
-            {\normalsize \ 
+            {\Large 
             Fernando Carvalho Pacheco\footnote{The University of Hawaii at Manoa}, 
             Fernando Santiago-Mandujano\footnotemark[\value{footnote}], 
             Albert Plueddemann\footnote{Woods Hole Oceanographic Institution},
@@ -222,9 +142,12 @@ latex_elements = {
             Daniel Fitzgerald\footnotemark[1] 
             and Nan Galbraith\footnotemark[2] \par}
             
-            \vspace{0.25cm}
-            {\normalsize \today\par} 
-            {\Large\bfseries Data Report - 16 \par}
+            \vspace{0.50cm}
+            {\normalsize \Large \today\par} 
+            
+            \vspace{1cm}
+            {\Huge\bfseries Data Report - 16 \par}
+            
             \vspace{1cm}
             
             {\normalsize \ Approved for public release; distribution unlimited. \par}
@@ -246,8 +169,8 @@ latex_elements = {
         \clearpage
         \pagenumbering{roman}
         \tableofcontents
-        % \listoffigures
-        % \listoftables
+        \listoffigures
+        \listoftables
         \clearpage
         \pagenumbering{arabic}
 
@@ -258,13 +181,13 @@ latex_elements = {
     'sphinxsetup': \
         'hmargin={0.7in,0.7in}, vmargin={1in,1in}, \
         verbatimwithframe=true, \
-        TitleColor={rgb}{0,0,0}',
-
+        TitleColor={rgb}{0,0,0}, \
+        HeaderFamily=\\rmfamily\\bfseries, \
+        InnerLinkColor={rgb}{0,0,1}, \
+        OuterLinkColor={rgb}{0,0,1}',
     'tableofcontents': ' ',
 
 }
-
-latex_additional_files = ['figures/*']
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -275,19 +198,8 @@ latex_logo = "_static/_images/all_whots_report.png"
 latex_documents = [
     ('index', 'whots16-data-report.tex',
      u'WHOTS-16: Data Report',
-     u'Fernando Carvalho Pacheco', 'report'),
+     u'Fernando Carvalho Pacheco', 'manual'),
 ]
-
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-# latex_use_parts = False
-
-# If true, show page references after internal links.
-# latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-# latex_show_urls = False
 
 # Documents to append as an appendix to all manuals.
 # latex_appendices = []
@@ -295,22 +207,14 @@ latex_documents = [
 # If false, no module index is generated.
 latex_domain_indices = True
 
-# -- Options for manual page output ---------------------------------------
-
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-# man_pages = [
-#     ('index', 'whots-manual-page-test', u'whots-manual-page-test',
-#      [u'Fernando Carvalho Pacheco'], 1)
-# ]
-#
-# latex_logo = "../source/_static/_images/logo_HOT.jpg"
-
 # -- Options for Epub output ----------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = u'whots-epub-test'
+epub_title = u'whots16-data-report.text'
 epub_author = u'Fernando Carvalho Pacheco'
 epub_publisher = u'Fernando Carvalho Pacheco'
 epub_copyright = f'2022-04-02-{datetime.now().year}, Fernando Carvalho Pacheco'
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
+
