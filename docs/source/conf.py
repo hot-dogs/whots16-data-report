6b89184492197efb54b8cabb9f9bcf57e2917b31
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-10-27 09:24:28 -1000

    version/release 0.0.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,8 @@ copyright = f'{datetime.now().year}, Hawaii Ocean Time-series (HOT)'
 author = 'Fernando Carvalho Pacheco, Fernando Santiago-Mandujano '
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
-version = '0.0.4'
+release = '0.0.5'
+version = '0.0.5'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
